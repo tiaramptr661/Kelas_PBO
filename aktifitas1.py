0d89e7a0454dd835473cbf79eb07b27886141954
Tiara Maulida Putri <79085153+tiaramptr661@users.noreply.github.com>
2021-02-26 10:18:25 +0700

    Create aktifitas1.py

---
+++ aktifitas1.py
@@ -0,0 +1,11 @@
+#INPUT
+
+nama=input("masukkan nama Anda")
+umur=19
+
+#OUTPUT
+print("")
+print("=====INPUT DAN OUTPUT PADA BAHASA PEMOGRAMAN PYTHON=====")
+print("nama Anda: ", nama)
+print("umur Anda: ", umur)
+print("")
