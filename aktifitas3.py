d4701502b09188ad0e65425e6cc1d0f666ce85ce
Tiara Maulida Putri <tiaramptr@upi.edu>
2021-02-26 17:54:53 +0700

    bismillah

---
+++ aktifitas3.py
@@ -0,0 +1,15 @@
+#PERCABANGAN
+print("percabangan pada bahasa pemograman Python")
+print("pilih bahasa pemograman: ")
+print("1. Python")
+print("2. C")
+
+pilih=int(input("pilih menu (1/2): "))
+
+if pilih == 1:
+    print("bahasa pemograman yang dipilih adalah Python")
+elif pilih == 2:
+    print("bahasa pemograman yang dipilih adalah C")
+else :
+    print("pilihan Anda tidak tersedia!!!")
+    
